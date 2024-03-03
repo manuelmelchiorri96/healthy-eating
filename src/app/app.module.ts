e5b42d401d0ca5d997306167a413e63d23455152
manuelmelchiorri96 <manuelmelchiorri1996@gmail.com>
2024-03-03 10:45:16 +0100

    aggiunto filtro per ricerca frutto e visualizzazione valori nutrizionali

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,12 +4,13 @@ import { HealthyEatingService } from '../../service/healthy-eating.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  fruits!: any[];
+  fruits: any[] = [];
   searchTerm: string = '';
-  filteredFruits!: any[];
+  filteredFruits: any[] = [];
+  selectedFruit: any | null = null;
 
   constructor(private healthyEatingService: HealthyEatingService) {}
 
@@ -17,12 +18,21 @@ export class HomeComponent {
     this.healthyEatingService.getAllFruits().subscribe({
       next: (data: any) => {
         this.fruits = data;
-        console.log(this.fruits);
-        console.log(data);
+        this.filteredFruits = this.fruits;
       },
       error: (error) => {
         console.error(error);
       },
     });
+  }
+
+  searchFruit() {
+    this.filteredFruits = this.fruits.filter((fruit) =>
+      fruit.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  toggleNutritionalInfo(fruit: any) {
+    this.selectedFruit = this.selectedFruit === fruit ? null : fruit;
   }
 }
